cf2c6c55f1c8e0f7eaf4ffc19452f44797ace9b3
detko <detkov.david05@mail.ru>
2025-11-06 01:05:23 +0300

    Добавил отображение фона

---
+++ FNAScreenServer/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new FNAScreenServer.Village();
+game.Run();
---
+++ FNAScreenServer/Village.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FNAScreenServer
+{
+    public class Village : Microsoft.Xna.Framework.Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D _villageBackground;
+        private Texture2D _snowflakeTexture;
+        public Village()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.IsFullScreen = false;
+            _graphics.ApplyChanges();
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _villageBackground = Content.Load<Texture2D>("Shrek");
+            _snowflakeTexture = Content.Load<Texture2D>("Snowflake");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(_villageBackground,
+                new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight),
+                Color.White);
+
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
